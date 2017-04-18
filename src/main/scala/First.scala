6c28e2c6222df935f44cf811e0a3a1fffbf475bb
Marcos Savoury <msavoury@carecloud.com>
2017-04-18 11:46:43 -0400

    Initial commit

---
+++ src/main/scala/First.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by msavoury on 4/18/17.
+  */
+object First extends App {
+
+  println("Hello World")
+
+  alpha("Marcos")
+
+  def alpha(str:String) = {
+    println(s"Hello ${str}")
+  }
+
+}
